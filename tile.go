55188e53b45f0d10a258804bf4f46453753f78e3
Homin Lee <homin.lee@suapapa.net>
2020-06-22 17:18:05 +0900

    cache tiles

--- tile.go
+++ tile.go
@@ -21,25 +21,34 @@ const (
 
 var (
 	tilesImage *ebiten.Image
+
+	tileMap map[int]*ebiten.Image
 )
 
+func init() {
+	r, err := os.Open("resource/tiles.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	img, _, err := image.Decode(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tilesImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+
+	tileMap = make(map[int]*ebiten.Image)
+}
+
 func tile(t int) *ebiten.Image {
-	if tilesImage == nil {
-		r, err := os.Open("resource/tiles.png")
-		if err != nil {
-			log.Fatal(err)
-		}
-		img, _, err := image.Decode(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		tilesImage, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+	if _, ok := tileMap[t]; !ok {
+		sx := (t % tileXNum) * tileSize
+		sy := (t / tileXNum) * tileSize
+
+		newTile := tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image)
+		tileMap[t] = newTile
 	}
 
-	sx := (t % tileXNum) * tileSize
-	sy := (t / tileXNum) * tileSize
-
-	return tilesImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image)
+	return tileMap[t]
 }
 
 func drawTile(screen *ebiten.Image, x, y int, t int) {
